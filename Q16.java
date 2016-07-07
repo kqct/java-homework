d987210072177342906b7b2092258d5cb1a65679
meow10811 <meow10811+github@gmail.com>
2016-07-07 16:30:11 -0400

    Finished problem

--- Q16.java
+++ Q16.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 public class Q16 {
     public static void main(String[] args) {
         System.out.println("    *What's your name?");
-        Scanner input = new Scanner(System.in);
-        String name = input.nextLine();
+        Scanner nameInput = new Scanner(System.in);
+        String name = nameInput.nextLine();
         System.out.println("    *Hello, " + name + "! How old are you?");
+        Scanner ageInput = new Scanner(System.in);
+        String age = ageInput.nextLine();
+        System.out.println("    *You're " + age + "? Congratulations!");
+        System.out.println("    *How much do you make, " + name + "?");
+        Scanner wageInput = new Scanner(System.in);
+        String wage = ageInput.nextLine();
+        System.out.println("    *"+wage+", huh? I hope that's in US dollars!");
     }
 }
