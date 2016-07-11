a97da72116fcedeae79a7ce8aaca9fe27b01d9fd
meow10811 <meow10811+github@gmail.com>
2016-07-11 16:38:06 -0400

    Completed Q21-Q26, started Q27

---
+++ Q27.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Q27{
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Are you ready for a quiz? ");
+        keyboard.nextLine();
+        System.out.println("Okay, here it comes!");
+        
+    }
+}
